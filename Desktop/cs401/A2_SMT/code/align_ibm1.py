5a052b318db45ec0c6ccd8081a761313557c58f1
Robert Bazzocchi <robertbazzocchi@gmail.com>
2018-03-08 03:10:29 -0500

    align begun

--- Desktop/cs401/A2_SMT/code/align_ibm1.py
+++ Desktop/cs401/A2_SMT/code/align_ibm1.py
@@ -27,7 +27,7 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
     AM = {}
     
     # Read training data
-    
+    read_hansard(train_dir, num_sentences)
     
     # Initialize AM uniformly
 
@@ -54,7 +54,39 @@ def read_hansard(train_dir, num_sentences):
 	
 	Make sure to read the files in an aligned manner.
 	"""
-    # TODO
+    pair_num = 1
+    processed_sentences = {}   
+    for subdir, dirs, files in os.walk(train_dir):
+        for file in files:
+            if file == ".DS_Store":
+                continue
+            if file.endswith(".e"): # DETERMINE THE LANGUAGE AND SET THE LANGUAGE VARIABLE
+            	language = "e"
+            	processed_sentences[pair_num] = {language: {}} # INITIALIZE THE NEXT DICT TO CONTAIN THE (E,F) PAIR OF SENTENCES
+            if file.endswith(".f"):
+            	language = "f"
+            	processed_sentences[pair_num][language] = {} # INITIALIZE THE NEXT DICT TO CONTAIN THE (E,F) PAIR OF SENTENCES
+            if file.endswith(".txt"):
+            	continue # ACCOUNTS FOR .txt FILE IN TRAINING FOLDER
+            path = train_dir+file
+            hansard_file = open(path,'r')
+
+            i = num_sentences # KEEPING TRACK OF THE NUMBER OF SENTENCES PROCESSED
+            for sentence in hansard_file.readlines():
+            	if i > 0:
+            		processed_sentences[pair_num][language][i] = preprocess(sentence,language)
+            		i-=1
+            		continue
+            	break
+            if language == "f": # UPDATE PAIRS IF THE F OF THE (E,F) PAIR HAS BEEN PROCESSED
+            	pair_num +=1 
+
+    # processed_sentences IS A DICT WITH:
+    # KEYS: FILE NUMBER 	 &		VALUES: DICTIONARIES (SEE BELOW)
+    # SUBKEYS: LANGUAGE 	 & 		SUBVALUES: SENTENCE NUMBER IN THE FILE
+
+    print(processed_sentences[92]["e"])
+    print(processed_sentences[92]["f"])
 
 def initialize(eng, fre):
     """
@@ -69,3 +101,8 @@ def em_step(t, eng, fre):
 	Follows the pseudo-code given in the tutorial slides.
 	"""
 	# TODO
+
+align_ibm1("../data/Hansard/Training/", 2, 5, "AM")
+
+
+
